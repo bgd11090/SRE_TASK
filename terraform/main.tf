7f25354cba09c119e17040ac8d75d388f1153dc8
Djordje <djordje.petrovic@htecgroup.com>
2026-02-28 02:27:05 +0100

    add module wiring for ecs taskdef

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,4 +1,10 @@
 module "ecs" {
-  source       = "./ecs_module"
-  cluster_name = var.cluster_name
+  source         = "./ecs_module"
+  cluster_name   = var.cluster_name
+  image          = var.image
+  container_name = var.container_name
+  cpu            = var.cpu
+  memory         = var.memory
+  port           = var.port
+  app_env        = var.app_env
 }
