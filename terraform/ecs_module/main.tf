317e710622c841d4d86074f4e0590baff4dbde34
Djordje <djordje.petrovic@htecgroup.com>
2026-02-28 02:54:49 +0100

    adding IAM execution role and policy attachment and execution role arn in taskdef

--- terraform/ecs_module/main.tf
+++ terraform/ecs_module/main.tf
@@ -14,12 +14,38 @@ resource "aws_cloudwatch_log_group" "app_logs" {
   }
 }
 
+resource "aws_iam_role" "ecs_task_execution_role" {
+  name = "ecsTaskExecutionRole-${var.container_name}"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Sid    = ""
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+      },
+    ]
+  })
+  tags = {
+    SRE_TASK = "Djordje Petrovic"
+  }
+}
+
+resource "aws_iam_role_policy_attachment" "ecs_task_execution_policy" {
+  role       = aws_iam_role.ecs_task_execution_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+}
+
 resource "aws_ecs_task_definition" "app" {
   family                   = var.container_name
   requires_compatibilities = ["FARGATE"]
   network_mode             = "awsvpc"
   cpu                      = var.cpu
   memory                   = var.memory
+  execution_role_arn       = aws_iam_role.ecs_task_execution_role.arn
   container_definitions    = jsonencode([
     {
       name      = var.container_name
